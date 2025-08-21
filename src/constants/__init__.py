8d93b4719415f9989bd994076e7975ce3572f35b
Noman <nmazhar911@gmail.com>
2025-08-21 06:17:43 +0500

    configuring column in yaml file to work on and also setting up constant to use later

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -14,6 +14,8 @@ ARTIFACT_DIR: str = "artifact"
 FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml") 
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -22,3 +24,8 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.25
+
+
+# Data Validation related constants
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
