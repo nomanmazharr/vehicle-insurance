c88536e4887ab0ed4c9cc50d74576ce6ec249c63
Noman <nmazhar911@gmail.com>
2025-08-23 11:09:41 +0500

    setting up constants config and artifact for model trainer

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -10,6 +10,10 @@ MONGODB_URL_KEY = "MONGO_URI"
 PIPELINE_NAME: str = ""
 ARTIFACT_DIR: str = "artifact"
 
+
+MODEL_FILE_NAME ="model.pkl"
+
+
 PREPROCESSING_OBJECT_FILE_NAME = 'preprocessing.pkl'
 TARGET_COLUMN: str = "Response"
 CURRENT_YEAR = date.today().year
@@ -37,3 +41,17 @@ DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = 'transformed'
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+
+# Model Trainer related constants
+MODEL_TRAINER_DIR_NAME: str = 'model_trainer'
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+MODEL_TRAINER_N_ESTIMATORS = 200
+MODEL_TRAINER_MIN_SAMPLES_SPLIT: int = 7
+MODEL_TRAINER_MIN_SAMPLES_LEAF: int = 6
+MIN_SAMPLES_SPLIT_MAX_DEPTH: int = 10
+MIN_SAMPLES_SPLIT_CRITERION: str = 'entropy'
+MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
